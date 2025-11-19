4324facd0b845c9d4cd402e82bf00124cf390f96
Shrikanth Hegde <sshegde@linux.ibm.com>
2025-11-19 18:14:49 +0530

    sysfs: disable arch handling if paravirt file being written
    Arch specific code can update the mask based on the steal time. For
    debugging it is desired to overwrite the arch logic. Do that with this
    debug patch.
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.ibm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10949,6 +10949,7 @@ void sched_enq_and_set_task(struct sched_enq_and_set_ctx *ctx)
 #ifdef CONFIG_PARAVIRT
 struct cpumask __cpu_paravirt_mask __read_mostly;
 EXPORT_SYMBOL(__cpu_paravirt_mask);
+DEFINE_STATIC_KEY_FALSE(disable_arch_paravirt_handling);
 
 static DEFINE_PER_CPU(struct cpu_stop_work, pv_push_task_work);
 
